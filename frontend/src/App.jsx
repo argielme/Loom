807f86c105cc5c9ad2794b33da6f5f83e0803af0
Argiel Ducut <argiel.em@gmail.com>
2026-07-27 22:01:07 +0800

    feat: Route setup for auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return(
     <>
-      <h1>Hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Hello world</h1>} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
