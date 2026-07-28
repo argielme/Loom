d07a2101a943180a78a10c88ba5019df97592134
Argiel Ducut <argiel.em@gmail.com>
2026-07-28 08:13:54 +0800

    refactor: Removed extra http method

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,6 +13,43 @@ router.post("/sign-in", async (req, res) => {
         message: "All fields are required",
       });
     }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        username: user.username,
+      },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    return res.status(200).json({
+      success: true,
+      token,
+      user: {
+        username: user.username,
+      },
+    });
   } catch (error) {
     return res.status(500).json({
       message: "Server error",
@@ -68,59 +105,5 @@ router.post("/sign-up", async (req, res) => {
     });
   }
 });
-
-router.post("/sign-in", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-      return res.status(400).json({
-        message: "All fields are required",
-      });
-    }
-
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid email or password",
-      })
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return res.status(400).json({
-        success: false,
-        message: "Invalid password",
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        id: user._id,
-        username: user.username,
-      },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "7d",
-      },
-    );
-
-    return res.status(200).json({
-      success: true,
-      token,
-      user: {
-        username: user.username,
-      },
-    })
-
-  } catch (error) {
-    return res.status(500).json({
-      message: "Server error",
-    });
-  }
-})
 
 module.exports = router;
