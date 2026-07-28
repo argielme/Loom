2cf8e1ed000fc0ce4b493e56e0546813470671b6
Argiel Ducut <argiel.em@gmail.com>
2026-07-28 08:08:13 +0800

    chore: Setup mongoose for db connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+const auth = require("./routes/auth");
+const connectDB = require("./config/db");
+
+connectDB();
+
+app.use("/auth", auth);
 
 const port = 3000;
 
