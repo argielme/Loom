a8d052e7b7974ea75bc7ea12ce60e67c37f02442
Argiel Ducut <argiel.em@gmail.com>
2026-07-28 16:38:01 +0800

    chore: middlewares to middleware

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,23 @@ app.use(express.json());
 app.use(cors());
 
 const auth = require("./routes/auth");
+const authMiddleware = require("./middleware/authMiddleware");
 const connectDB = require("./config/db");
 
 connectDB();
 
 app.use("/auth", auth);
+app.use("/auth-middleware", authMiddleware);
 
 const port = 3000;
 
 app.listen(port, () => {
   console.log(`Port: ${port}`);
+});
+
+app.get("/auth-middleware", authMiddleware, (req, res) => {
+  res.json({
+    message: "Token is valid!",
+    user: req.user,
+  });
 });
